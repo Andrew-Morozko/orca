fb99becc2ed81a9d86894ddbf949685b93e9aafd
Andrew Morozko <Andrew-Morozko@users.noreply.github.com>
2019-10-31 17:08:30 +0300

    Cleanup build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 cd "$( dirname "${BASH_SOURCE[0]}" )/.."
 
-env go build -v -o orca-release/orca
-retVal=$?
-if [ $retVal -ne 0 ]; then
-    exit $retVal
-fi
+go build -v -o orca-release/orca
+exit $?
